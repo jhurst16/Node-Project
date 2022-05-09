eabd258e558673b5aa22e64925c2c76b753ce230
Jeremy's commits <jeremyhurst16@gmail.com>
2022-05-09 11:14:31 -0400

    added middleware for handlebars, main and login views/layouts and added corresponding routes. added static stylesheet option to app

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
+const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
+const exphbs = require("express-handlebars");
 const connectDB = require("./config/db");
 
-//load config
+// load config
 dotenv.config({ path: "./config/config.env" });
 
 connectDB();
 
 const app = express();
+
+// logging
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+// Handlebars
+app.engine(".hbs", exphbs.engine({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", ".hbs");
+
+// Static Fodler
+app.use(express.static(path.join(__dirname, "public")));
+
+// Routes
+app.use("/", require("./routes/index"));
 
 const PORT = process.env.PORT || 3000;
 
